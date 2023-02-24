48ce6b95c06242f52584f5dbbd627ed173e2c502
ThalesJC <thales.soul96@gmail.com>
2023-02-24 16:27:29 -0300

    feat: search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -23,6 +23,10 @@ def search_by_date(date):
         raise ValueError("Data inválida")
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    return [
+        (news["title"], news["url"])
+        for news in search_news(
+            {"category": {"$regex": category, "$options": "i"}}
+        )
+    ]
