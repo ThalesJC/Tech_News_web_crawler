6100cec4e007a6214cb38ca0e66efebd15b50ae2
ThalesJC <thales.soul96@gmail.com>
2023-02-28 19:15:22 -0300

    feat: top_five

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -2,5 +2,10 @@ from tech_news.database import find_news
 
 
 def top_5_categories():
-    result = find_news()
-    return print(result)
+    news = find_news()
+    categories = [new["category"] for new in news]
+    counter = {}
+    for category in categories:
+        counter[category] = counter.get(category, 0) + 1
+    top_5 = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    return [category for category, count in top_5[:5]]
